7116f6b06f2c4fb520b8aa58a53a02510dbe2a45
zhouxu <01153044@yto.net.cn>
2017-12-26 15:00:55 +0800

    Commit

--- rpc-core/src/main/java/org/cyabird/core/task/StandardThreadExecutor.java
+++ rpc-core/src/main/java/org/cyabird/core/task/StandardThreadExecutor.java
@@ -1,21 +1,18 @@
 package org.cyabird.core.task;
 
+import org.cyabird.core.task.support.ExecutorQueue;
+import org.cyabird.core.task.support.ThrottleSupport;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.util.Assert;
-import org.springframework.util.ConcurrencyThrottleSupport;
 
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.*;
 
 /**
  * StandardThreadExecutor execute执行策略：	优先扩充线程到maxThread，再offer到queue，如果满了就reject
  * <p>
  * 适应场景：  比较适合于业务处理需要远程资源的场景
  */
-public class StandardThreadExecutor {
+public class StandardThreadExecutor extends ThreadPoolTaskExecutor {
 
     /**
      * 核心线程池大小
@@ -53,10 +50,7 @@ public class StandardThreadExecutor {
     private static final int DEFAULT_KEEP_ALIVE_SECONDS = 60;
 
     /** 线程额外对象 */
-    private final ThrottleSupport throttleSupport = new ThrottleSupport();
-
-    /** 线程池对象 */
-    private final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+    private final ConcurrencyThrottleAdapter throttleSupport = new ConcurrencyThrottleAdapter();
 
     public StandardThreadExecutor() {
         this(DEFAULT_CORE_POOL_SIZE, DEFAULT_MAX_POOL_SIZE);
@@ -79,88 +73,92 @@ public class StandardThreadExecutor {
     }
 
     public StandardThreadExecutor(int corePoolSize, int maxPoolSize, int keepAliveSeconds, int queueCapacity, ThreadFactory threadFactory, RejectedExecutionHandler rejectedExecutionHandler) {
-        threadPoolTaskExecutor.setCorePoolSize(corePoolSize);
-        threadPoolTaskExecutor.setMaxPoolSize(maxPoolSize);
-        threadPoolTaskExecutor.setQueueCapacity(queueCapacity);
-        threadPoolTaskExecutor.setKeepAliveSeconds(keepAliveSeconds);
-        threadPoolTaskExecutor.setThreadFactory(threadFactory);
-        threadPoolTaskExecutor.setRejectedExecutionHandler(rejectedExecutionHandler);
+        super.setCorePoolSize(corePoolSize);
+        super.setMaxPoolSize(maxPoolSize);
+        super.setKeepAliveSeconds(keepAliveSeconds);
+        super.setThreadFactory(threadFactory);
+        super.setRejectedExecutionHandler(rejectedExecutionHandler);
         // 设置最大并发数 maxPoolSize + queueCapacity
         throttleSupport.setConcurrencyLimit(maxPoolSize + queueCapacity);
-        threadPoolTaskExecutor.initialize();
+        initialize();
     }
 
+    @Override
     public void execute(Runnable task) {
-        Assert.state(threadPoolTaskExecutor != null, "线程池没有初始化");
-        throttleSupport.beforeAccess();
-        threadPoolTaskExecutor.execute(() -> {
+        throttleSupport.before(task);
+        super.execute(() -> {
             try {
                 task.run();
+            } catch (RejectedExecutionException rx) {
+                // 尝试放入队列，失败则直接使用失败策略
+                if (!((ExecutorQueue) super.getThreadPoolExecutor().getQueue()).force(task)) {
+                    super.getThreadPoolExecutor().getRejectedExecutionHandler().rejectedExecution(task, super.getThreadPoolExecutor());
+                }
             } finally {
-                throttleSupport.afterAccess();
+                throttleSupport.after();
             }
         });
     }
 
-    public void destroy() {
-        Assert.state(threadPoolTaskExecutor != null, "线程池没有初始化");
-        throttleSupport.awaitFinish();
-        threadPoolTaskExecutor.destroy();
+    @Override
+    protected BlockingQueue<Runnable> createQueue(int queueCapacity) {
+        ExecutorQueue executorQueue = new ExecutorQueue();
+        executorQueue.setStandardThreadExecutor(this);
+        return executorQueue;
     }
 
-    private static class ThrottleSupport extends ConcurrencyThrottleSupport {
+    /**
+     * 获取运行任务数量
+     *
+     * @return
+     */
+    public int getConcurrencyCount() {
+        return throttleSupport.getConcurrencyCount().get();
+    }
 
-        /** 正在处理的任务数 */
-        public AtomicInteger submittedTasksCount;
+    private class ConcurrencyThrottleAdapter extends ThrottleSupport {
 
-        /** 锁对象 */
-        private Object monitor = new Object();
+        /** 并发量设置 */
+        private int concurrencyLimit;
 
-        /** 是否异常 */
-        private boolean interrupted = false;
+        /** Executor */
+        private ThreadPoolExecutor threadPoolExecutor = getThreadPoolExecutor();
 
         /**
          * 运行前
+         * <p>
+         * 默认队列没有长度限制，因此这里进行控制
          *
-         * @see ConcurrencyThrottleSupport#beforeAccess()
+         * @see ThrottleSupport#beforeAccess()
          */
-        @Override
-        protected void beforeAccess() {
-            super.beforeAccess();
-            submittedTasksCount.getAndIncrement();
+        protected void before(Runnable task) {
+            int count = super.beforeAccess();
+            // 因为队列没有长度，所以在这里进行并发控制
+            if (count > concurrencyLimit) {
+                getConcurrencyCount().decrementAndGet();
+                /**
+                 * @see java.util.concurrent.ThreadPoolExecutor.AbortPolicy#rejectedExecution(Runnable, ThreadPoolExecutor)
+                 */
+                threadPoolExecutor.getRejectedExecutionHandler().rejectedExecution(task, threadPoolExecutor);
+            }
         }
 
         /**
          * 运行后
          *
-         * @see ConcurrencyThrottleSupport#afterAccess()
+         * @see ThrottleSupport#afterAccess()
          */
-        @Override
-        protected void afterAccess() {
+        protected void after() {
             super.afterAccess();
-            submittedTasksCount.getAndDecrement();
-            monitor.notify();
         }
 
         /**
-         * 等待所有线程完成
+         * 设置最大并发
          *
-         * @see #concurrencyCount
+         * @param concurrencyLimit
          */
-        public void awaitFinish() {
-            synchronized (monitor) {
-                // 如果超过,则无限循环此方法
-                while (submittedTasksCount.get() != 0) {
-                    try {
-                        // 线程等待
-                        this.monitor.wait();
-                    } catch (InterruptedException ex) {
-                        // 发生异常,尝试终止线程
-                        Thread.currentThread().interrupt();
-                        interrupted = true;
-                    }
-                }
-            }
+        protected void setConcurrencyLimit(int concurrencyLimit) {
+            this.concurrencyLimit = concurrencyLimit;
         }
     }
 
---
+++ rpc-core/src/main/java/org/cyabird/core/task/support/ThrottleSupport.java
@@ -0,0 +1,32 @@
+package org.cyabird.core.task.support;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @see org.springframework.util.ConcurrencyThrottleSupport
+ */
+public class ThrottleSupport {
+
+    /** 当前线程数量 （{@link ThreadPoolExecutor#getActiveCount()} 返回值并不准确） */
+    private AtomicInteger concurrencyCount = new AtomicInteger(0);
+
+    /** 运行前 */
+    protected int beforeAccess() {
+        return concurrencyCount.incrementAndGet();
+    }
+
+    /** 运行后 */
+    protected int afterAccess() {
+        return this.concurrencyCount.decrementAndGet();
+    }
+
+    /**
+     * 返回当前任务数量
+     *
+     * @return
+     */
+    public AtomicInteger getConcurrencyCount() {
+        return concurrencyCount;
+    }
+}
