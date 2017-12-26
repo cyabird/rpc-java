1469e3ff0a719fa04b6b41785e21a6bb07618aef
zhouxu <01153044@yto.net.cn>
2017-12-26 18:05:19 +0800

    Commit

--- rpc-core/src/test/java/org/cyabird/core/task/StandardThreadExecutorTest.java
+++ rpc-core/src/test/java/org/cyabird/core/task/StandardThreadExecutorTest.java
@@ -12,40 +12,45 @@ public class StandardThreadExecutorTest {
         StandardThreadExecutor standardThreadExecutor = new StandardThreadExecutor();
         standardThreadExecutor.execute(() -> {
             try {
+                System.out.println("测试1----开始");
                 Thread.sleep(10000);
-                System.out.println("测试1");
+                System.out.println("测试1----结束");
             }catch (InterruptedException e){
                 Thread.interrupted();
             }
         });
         standardThreadExecutor.execute(() -> {
             try {
-                System.out.println("测试2");
+                System.out.println("测试2----开始");
                 Thread.sleep(10000);
+                System.out.println("测试2----结束");
             }catch (InterruptedException e){
                 Thread.interrupted();
             }
         });
         standardThreadExecutor.execute(() -> {
             try {
-                System.out.println("测试3");
+                System.out.println("测试3----开始");
                 Thread.sleep(10000);
+                System.out.println("测试3----结束");
             }catch (InterruptedException e){
                 Thread.interrupted();
             }
         });
         standardThreadExecutor.execute(() -> {
             try {
-                System.out.println("测试4");
+                System.out.println("测试4----开始");
                 Thread.sleep(10000);
+                System.out.println("测试4----结束");
             }catch (InterruptedException e){
                 Thread.interrupted();
             }
         });
         standardThreadExecutor.execute(() -> {
             try {
-                System.out.println("测试5");
+                System.out.println("测试5----开始");
                 Thread.sleep(10000);
+                System.out.println("测试5----结束");
             }catch (InterruptedException e){
                 Thread.interrupted();
             }
